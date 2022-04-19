7570d777153f231add7c71ace34c259de965bb51
kiekk <kiekk@i-commtech.com>
2022-04-19 16:04:45 +0900

    string '.equals()' compare

--- src/stringPool/StringPool.java
+++ src/stringPool/StringPool.java
@@ -11,5 +11,9 @@ public class StringPool {
         System.out.printf("string1 == string2 : %s\n", (string1 == string2));
         System.out.printf("string2 == string3 : %s\n", (string2 == string3));
         System.out.printf("string3 == string4 : %s\n", (string3 == string4));
+        System.out.println("-------------------------------------------------");
+        System.out.printf("string1.equals(string2) : %s\n", (string1.equals(string2)));
+        System.out.printf("string2.equals(string3) : %s\n", (string2.equals(string3)));
+        System.out.printf("string3.equals(string4) : %s\n", (string3.equals(string4)));
     }
 }
