86ca39672cc28465ede09008d5ba16490d5c2be2
kiekk <kiekk@i-commtech.com>
2022-04-19 16:04:28 +0900

    string '==' compare

---
+++ src/stringPool/StringPool.java
@@ -0,0 +1,15 @@
+package stringPool;
+
+public class StringPool {
+
+    public static void main(String[] args) {
+        String string1 = "Baeldung";
+        String string2 = "Baeldung";
+        String string3 = new String("Baeldung");
+        String string4 = new String("Baeldung");
+
+        System.out.printf("string1 == string2 : %s\n", (string1 == string2));
+        System.out.printf("string2 == string3 : %s\n", (string2 == string3));
+        System.out.printf("string3 == string4 : %s\n", (string3 == string4));
+    }
+}
