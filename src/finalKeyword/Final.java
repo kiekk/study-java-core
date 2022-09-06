239947aab10fc34c84c01f8d1ac85bd9c15844c3
soonho <dbstnsgh2@naver.com>
2022-09-06 20:51:19 +0900

    final 변수

---
+++ src/finalKeyword/Final.java
@@ -0,0 +1,10 @@
+package finalKeyword;
+
+public class Final {
+    private final String finalString = "1234";
+    public static void main(String[] args) {
+        // final 변수는 초기화 후 변경 불가
+//        new Final().finalString = "1234";
+        // Cannot assign a value to final variable 'finalString
+    }
+}
