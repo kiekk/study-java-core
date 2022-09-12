4a3ec2103566f722ed70086dc8b323e0f56c25a4
soonho <dbstnsgh2@naver.com>
2022-09-12 20:24:29 +0900

    제네릭 - 2

--- src/generics/Generics.java
+++ src/generics/Generics.java
@@ -1,8 +1,16 @@
 package generics;
 
+import java.util.List;
+
 public class Generics {
     public static void main(String[] args) {
         Animal a = new Tiger(); // OK
         // Cage<Animal> ca = new Cage<Tiger>(); // COMPILE ERROR
+
+        Cage<Tiger> ct = new Cage<Tiger>();
+//        Cage<Animal> ca = ct; // 이 코드가 가능하다면
+//        ca.push(new Lion()); // Lion 또한 Animal 의 자식 클래스이기 때문에 가능
+//
+//        List<Tiger> tigers = ct.getAll(); // Lion 리스트를 반환하며 이를 Tiger 타입으로 저장하는 것도 가능해야 합니다.
     }
 }
