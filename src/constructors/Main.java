88c679c1900b8cc476839e74da29e0d5d2ab3396
kiekk <kiekk@i-commtech.com>
2022-09-20 10:42:03 +0900

    constructor: 생성자 동작 방식 테스트 코드 작성 - 3

--- src/constructors/Main.java
+++ src/constructors/Main.java
@@ -1,5 +1,7 @@
 package constructors;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         Developer developer = new Developer();
@@ -11,5 +13,9 @@ public class Main {
         Developer mary = new Developer("Mary", "Smith");
         // Developer{firstName='Mary', lastName='Smith', languages=null}
         System.out.println(mary);
+
+        Developer marcus = new Developer("Marcus", "Johnson", Arrays.asList("Java", "JavaScript"));
+        // Developer{firstName='Marcus', lastName='Johnson', languages=[Java, JavaScript]}
+        System.out.println(marcus);
     }
 }
